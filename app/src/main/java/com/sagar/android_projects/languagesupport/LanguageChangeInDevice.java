b10add26adf94002b84d987e1fa780a80cf29c48
sagar <snkumar.nayak@gmail.com>
2017-10-31 11:37:31 +0530

    commit

--- app/src/main/java/com/sagar/android_projects/languagesupport/LanguageChangeInDevice.java
+++ app/src/main/java/com/sagar/android_projects/languagesupport/LanguageChangeInDevice.java
@@ -13,12 +13,17 @@ import android.widget.TextView;
 
 /**
  * created by SAGAR KUMAR NAYAK on 31 OCT 2017.
- * this is a
+ * this is an activity to show language selection when we want to change the language of the entire
+ * device.
+ * for this purpose we have to start the intent to start the settings activity.
  */
 public class LanguageChangeInDevice extends AppCompatActivity {
 
+    @SuppressWarnings("FieldCanBeLocal")
     private Button buttonChangeLanguage;
+    @SuppressWarnings("FieldCanBeLocal,unused")
     private TextView textViewInstruction;
+    @SuppressWarnings("FieldCanBeLocal,unused")
     private EditText editText;
 
     @Override
@@ -28,10 +33,16 @@ public class LanguageChangeInDevice extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //view binding
         buttonChangeLanguage = findViewById(R.id.button_change_language);
         textViewInstruction = findViewById(R.id.textview_instruction);
         editText = findViewById(R.id.edittext);
+        ////////////////////////////////////////////////////////////////////////////////////////////
 
+        /*
+        on click for button to start settings activity to change the device default language.
+         */
         buttonChangeLanguage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +54,12 @@ public class LanguageChangeInDevice extends AppCompatActivity {
         });
     }
 
+    /**
+     * method to be fired when there is changes in configuration. if the configuration is changed with
+     * the language this method will be fired.
+     * here we have to start the activity again so that the changes may take place.
+     * @param newConfig new configuration
+     */
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
